fcb24c4d0e84d78b4913b4005f741606fe63d907
newb-ver4 <shutdown2423@gmail.com>
2023-12-27 17:30:21 +0900

    first commit

---
+++ clean/clean/src/main/java/com/clean/config/WebConfiguration.java
@@ -0,0 +1,43 @@
+package com.clean.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = { "com.clean.controller" })
+public class WebConfiguration implements WebMvcConfigurer {
+	
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+	
+	 
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+	}
+
+
+	@Bean
+	public CommonsMultipartResolver multipartResolver() {
+		
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		multipartResolver.setDefaultEncoding("utf-8");
+		multipartResolver.setMaxInMemorySize(2*1024*1024);
+		multipartResolver.setMaxUploadSize(10*1024*1024);
+		
+		return multipartResolver;
+	}
+	
+}
---
+++ clean/clean/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>home</title>
+</head>
+<body>
+	<div class="ml-auto">
+        <a href="/clean/account/register" style="color: inherit;">
+            <span class="mb-0 text font-weight-bold">회원가입</span>
+        </a>
+    </div>
+</body>
+</html>
